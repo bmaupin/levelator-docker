90330b97aa27a8fcac475fe4ff8b040b41ff96fc
bmaupin <bmaupin@users.noreply.github.com>
2021-02-15 12:25:20 -0500

    feat: Remove python dependency
    This makes for a much smaller image and much faster file processing time

--- levelator.sh
+++ levelator.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # Pass the full path of the input and output files to levelator
-python2.5 INSTALLDIR/.levelator/levelator.py $(readlink -f "$1") $(readlink -f "$2")
+/usr/local/bin/level $(readlink -f "$1") $(readlink -f "$2") $(echo -n 'The Levelator copyright (c) 2006 by GigaVox Media, Inc. and Singular Software'$(( `date +%s` / 10 ))0 | md5sum | cut -f 1 -d ' ')
